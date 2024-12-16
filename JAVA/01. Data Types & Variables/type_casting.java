761805fec7c8b8dd1241c6e916edd82e8739d881
janitkapadiya <janitkapdiya@gmail.com>
2024-12-16 15:08:38 +0530

    Update

---
+++ JAVA/01. Data Types & Variables/type_casting.java
@@ -0,0 +1,13 @@
+public class type_casting {
+  public static void main(String arg[]) {
+    // Example 1
+    float a = 12.24f;
+    int b = (int) a;
+    System.out.println(b);
+
+    // Example 2
+    char name = 'J';
+    int name_to_number = (int) name;
+    System.out.println(name_to_number);
+  }
+}
